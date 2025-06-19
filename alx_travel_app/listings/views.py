1bbb7782a8880ae4e9daafdb2ebc1bf8b11e90ee
kal-kyokya <kalkyokya4@gmail.com>
2025-06-19 11:46:41 +0000

    Create ViewSets for Listings and Bookings.

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+
+# --------------------------------------------------
+# Full CRUD for 'Listing' model out of the box.
+# --------------------------------------------------
+class ListingViewSet(ModelViewSet):
+    """Handles API calls to '/listings'.
+    Inheritance:
+    	ModelViewSet: Collection of Django predefined CRUD executing methods and required attributes.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+# --------------------------------------------------
+# Full CRUD for 'Booking' model out of the box.
+# --------------------------------------------------
+class BookingViewSet(ModelViewSet):
+    """Handles API calls to '/bookings'.
+    Inheritance:
+    	ModelViewSet: Collection of Django predefined CRUD executing methods and required attributes.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
